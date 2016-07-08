3f3d1361f1dd75ad76c47f1ee926a97bb36f6508
Sanjeeb Talukdar <sanjeeb.talukdar@gmail.com>
2016-07-08 16:01:02 +0530

    Updated @BeforeClass & @AfterClass

--- src/test/java/com/training/sample/SchoolFirstDayTest.java
+++ src/test/java/com/training/sample/SchoolFirstDayTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.training.sample.SchoolFirstDay;
@@ -13,17 +15,31 @@ public class SchoolFirstDayTest {
 	SchoolFirstDay school = new SchoolFirstDay();
 	String[] bag1 = { "Books", "Notebooks", "Pens" };
 	String[] bag2 = { "Books", "Notebooks", "Pens", "Pencils" };
+	
+	@BeforeClass
+	// Will be performed before all test.
+	public void testBeforeClass()
+	{
+		System.out.println("Setup for test complete.");
+	}
+
+	@AfterClass
+	// Will be performed after all test.
+	public void testAfterClass()
+	{
+		System.out.println("Test complete.");
+	}
 
 	@Before
 	// Will be performed before each test.
-	public void testSetup()
+	public void testBrforeEachTest()
 	{
 		System.out.println("Setup for test complete.");
 	}
 
 	@After
 	// Will be performed after each test.
-	public void testComplete()
+	public void testAfterEachTest()
 	{
 		System.out.println("Test complete.");
 	}
