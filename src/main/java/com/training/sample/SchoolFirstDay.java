a9c82103a2c4fc8415660f40d8431ea700b88177
ponmalarkiruba <ponmalar2806@gmail.com>
2016-07-22 11:14:24 +0530

    Update SchoolFirstDay.java

--- src/main/java/com/training/sample/SchoolFirstDay.java
+++ src/main/java/com/training/sample/SchoolFirstDay.java
@@ -1,18 +1,32 @@
 package com.training.sample;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class SchoolFirstDay {
+	
+	String [] schoolbag= null;
 
 	public String[] prepareMyBag() {
-		String[] schoolbag = { "Books", "Notebooks", "Pens" };
+		schoolbag = new String[] { "Books", "Notebooks", "Pens" };
 		System.out.println("My school bag contains: " + Arrays.toString(schoolbag));
 		return schoolbag;
 	}
 
 	public String[] addPencils() {
-		String[] schoolbag = { "Books", "Notebooks", "Pens", "Pencils" };
+		schoolbag = new String[] { "Books", "Notebooks", "Pens", "Pencils" };
 		System.out.println("Now my school bag contains: " + Arrays.toString(schoolbag));
+		return schoolbag;
+	}
+	
+	public String[] sortItems()
+	{
+		List<String> wordList = new ArrayList<String>(); 
+		wordList= Arrays.asList(schoolbag);  
+		Collections.sort(wordList,(s1, s2) -> s1.compareTo(s2));
+		schoolbag= wordList.toArray(schoolbag);
 		return schoolbag;
 	}
 }
